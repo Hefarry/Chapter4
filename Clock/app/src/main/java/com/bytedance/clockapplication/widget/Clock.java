1efda814b768a32b4940fa63d37f4861221c0252
He <1223922325@qq.com>
2019-07-05 17:37:17 +0800

    finish

---
+++ Clock/app/src/main/java/com/bytedance/clockapplication/widget/Clock.java
@@ -0,0 +1,293 @@
+package com.bytedance.clockapplication.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.text.Layout;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.text.style.RelativeSizeSpan;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+public class Clock extends View {
+
+    private final static String TAG = Clock.class.getSimpleName();
+
+    private static final int FULL_ANGLE = 360;
+
+    private static final int CUSTOM_ALPHA = 140;
+    private static final int FULL_ALPHA = 255;
+
+    private static final int DEFAULT_PRIMARY_COLOR = Color.WHITE;
+    private static final int DEFAULT_SECONDARY_COLOR = Color.LTGRAY;
+
+    private static final float DEFAULT_DEGREE_STROKE_WIDTH = 0.010f;
+
+    public final static int AM = 0;
+
+    private static final int RIGHT_ANGLE = 90;
+
+    private int mWidth, mCenterX, mCenterY, mRadius;
+
+    /**
+     * properties
+     */
+    private int centerInnerColor;
+    private int centerOuterColor;
+
+    private int secondsNeedleColor;
+    private int hoursNeedleColor;
+    private int minutesNeedleColor;
+
+    private int degreesColor;
+
+    private int hoursValuesColor;
+
+    private int numbersColor;
+
+    private boolean mShowAnalog = true;
+
+    public Clock(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public Clock(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public Clock(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int size;
+        int width = getMeasuredWidth();
+        int height = getMeasuredHeight();
+        int widthWithoutPadding = width - getPaddingLeft() - getPaddingRight();
+        int heightWithoutPadding = height - getPaddingTop() - getPaddingBottom();
+
+        if (widthWithoutPadding > heightWithoutPadding) {
+            size = heightWithoutPadding;
+        } else {
+            size = widthWithoutPadding;
+        }
+
+        setMeasuredDimension(size + getPaddingLeft() + getPaddingRight(), size + getPaddingTop() + getPaddingBottom());
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+
+        this.centerInnerColor = Color.LTGRAY;
+        this.centerOuterColor = DEFAULT_PRIMARY_COLOR;
+
+        this.secondsNeedleColor = DEFAULT_SECONDARY_COLOR;
+        this.hoursNeedleColor = DEFAULT_PRIMARY_COLOR;
+        this.minutesNeedleColor = DEFAULT_PRIMARY_COLOR;
+
+        this.degreesColor = DEFAULT_PRIMARY_COLOR;
+
+        this.hoursValuesColor = DEFAULT_PRIMARY_COLOR;
+
+        numbersColor = Color.WHITE;
+    }
+
+    @Override
+    protected void onDraw(final Canvas canvas) {
+        super.onDraw(canvas);
+
+        mWidth = getHeight() > getWidth() ? getWidth() : getHeight();
+
+        int halfWidth = mWidth / 2;
+        mCenterX = halfWidth;
+        mCenterY = halfWidth;
+        mRadius = halfWidth;
+
+        if (mShowAnalog) {
+            drawDegrees(canvas);
+            drawHoursValues(canvas);
+            drawNeedles(canvas);
+            drawCenter(canvas);
+        } else {
+            drawNumbers(canvas);
+        }
+        postInvalidateDelayed(1000);
+    }
+
+    private void drawDegrees(Canvas canvas) {
+
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setStrokeWidth(mWidth * DEFAULT_DEGREE_STROKE_WIDTH);
+        paint.setColor(degreesColor);
+
+        int rPadded = mCenterX - (int) (mWidth * 0.01f);
+        int rEnd = mCenterX - (int) (mWidth * 0.05f);
+
+        for (int i = 0; i < FULL_ANGLE; i += 6 /* Step */) {
+
+            if ((i % RIGHT_ANGLE) != 0 && (i % 15) != 0)
+                paint.setAlpha(CUSTOM_ALPHA);
+            else {
+                paint.setAlpha(FULL_ALPHA);
+            }
+
+            int startX = (int) (mCenterX + rPadded * Math.cos(Math.toRadians(i)));
+            int startY = (int) (mCenterX - rPadded * Math.sin(Math.toRadians(i)));
+
+            int stopX = (int) (mCenterX + rEnd * Math.cos(Math.toRadians(i)));
+            int stopY = (int) (mCenterX - rEnd * Math.sin(Math.toRadians(i)));
+
+            canvas.drawLine(startX, startY, stopX, stopY, paint);
+
+        }
+    }
+
+    /**
+     * @param canvas
+     */
+    private void drawNumbers(Canvas canvas) {
+
+        TextPaint textPaint = new TextPaint();
+        textPaint.setTextSize(mWidth * 0.2f);
+        textPaint.setColor(numbersColor);
+        textPaint.setColor(numbersColor);
+        textPaint.setAntiAlias(true);
+
+        Calendar calendar = Calendar.getInstance();
+
+        int hour = calendar.get(Calendar.HOUR);
+        int minute = calendar.get(Calendar.MINUTE);
+        int second = calendar.get(Calendar.SECOND);
+        int amPm = calendar.get(Calendar.AM_PM);
+
+        String time = String.format("%s:%s:%s%s",
+                String.format(Locale.getDefault(), "%02d", hour),
+                String.format(Locale.getDefault(), "%02d", minute),
+                String.format(Locale.getDefault(), "%02d", second),
+                amPm == AM ? "AM" : "PM");
+
+        SpannableStringBuilder spannableString = new SpannableStringBuilder(time);
+        spannableString.setSpan(new RelativeSizeSpan(0.3f), spannableString.toString().length() - 2, spannableString.toString().length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE); // se superscript percent
+
+        StaticLayout layout = new StaticLayout(spannableString, textPaint, canvas.getWidth(), Layout.Alignment.ALIGN_CENTER, 1, 1, true);
+        canvas.translate(mCenterX - layout.getWidth() / 2f, mCenterY - layout.getHeight() / 2f);
+        layout.draw(canvas);
+    }
+
+    /**
+     * Draw Hour Text Values, such as 1 2 3 ...
+     *
+     * @param canvas
+     */
+    private void drawHoursValues(Canvas canvas) {
+        // Default Color:
+        // - hoursValuesColor
+        TextPaint textPaint = new TextPaint();
+        textPaint.setTextSize(80);
+        textPaint.setColor(hoursNeedleColor);
+        textPaint.setStyle(Paint.Style.FILL);
+        textPaint.setTextAlign(Paint.Align.CENTER);
+
+        Paint.FontMetrics fontMetrics = textPaint.getFontMetrics();
+        float top = fontMetrics.top;
+        float bottom = fontMetrics.bottom;
+
+        String[] time = {"12","01","02","03","04","05","06","07","08","09","10","11"};
+        for(int i = 0;i < FULL_ANGLE;i += 30){
+            int cx = (int) (mCenterX + 400 * Math.sin(Math.toRadians(i)));
+            int cy = (int) (mCenterX - 400 * Math.cos(Math.toRadians(i)));
+            int baseLineY = (int) (cy - top/2 - bottom/2);
+            canvas.drawText(time[i/30],cx,baseLineY,textPaint);
+        }
+    }
+
+    /**
+     * Draw hours, minutes needles
+     * Draw progress that indicates hours needle disposition.
+     *
+     * @param canvas
+     */
+    private void drawNeedles(final Canvas canvas) {
+        // Default Color:
+        // - secondsNeedleColor
+        // - hoursNeedleColor
+        // - minutesNeedleColor
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR);
+        int minute = calendar.get(Calendar.MINUTE);
+        int second =  calendar.get(Calendar.SECOND);
+
+        Paint sNeedle = new Paint();
+        sNeedle.setColor(secondsNeedleColor);
+        sNeedle.setStyle(Paint.Style.FILL);
+        sNeedle.setStrokeWidth(6);
+        int sLength = 360;
+        int sEndX = (int) (mCenterX + sLength * Math.sin(Math.toRadians(second * 6)));
+        int sEndY = (int) (mCenterX - sLength * Math.cos(Math.toRadians(second * 6)));
+        canvas.drawLine(mCenterX,mCenterX,sEndX,sEndY,sNeedle);
+
+        Paint mNeedle = new Paint();
+        mNeedle.setColor(minutesNeedleColor);
+        mNeedle.setStyle(Paint.Style.FILL);
+        mNeedle.setStrokeWidth(8);
+        int mLength = 320;
+        int mEndX = (int) (mCenterX + mLength * Math.sin(Math.toRadians(minute * 6)));
+        int mEndY = (int) (mCenterX - mLength * Math.cos(Math.toRadians(minute * 6)));
+        canvas.drawLine(mCenterX,mCenterX,mEndX,mEndY,mNeedle);
+
+        Paint hNeedle = new Paint();
+        hNeedle.setColor(hoursNeedleColor);
+        hNeedle.setStyle(Paint.Style.FILL);
+        hNeedle.setStrokeWidth(12);
+        int hLength = 240;
+        int hEndX = (int) (mCenterX + hLength * Math.sin(Math.toRadians(hour * 30 + minute / 2)));
+        int hEndY = (int) (mCenterX - hLength * Math.cos(Math.toRadians(hour * 30 + minute / 2)));
+        canvas.drawLine(mCenterX,mCenterX,hEndX,hEndY,hNeedle);
+    }
+
+    /**
+     * Draw Center Dot
+     *
+     * @param canvas
+     */
+    private void drawCenter(Canvas canvas) {
+        // Default Color:
+        // - centerInnerColor
+        // - centerOuterColor
+        Paint paint1 = new Paint();
+        paint1.setColor(centerOuterColor);
+        paint1.setStyle(Paint.Style.STROKE);
+        paint1.setStrokeWidth(10);
+        canvas.drawCircle(mCenterX,mCenterX,20,paint1);
+
+        Paint paint2 = new Paint();
+        paint2.setColor(centerInnerColor);
+        paint2.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(mCenterX,mCenterX,20,paint2);
+    }
+
+    public void setShowAnalog(boolean showAnalog) {
+        mShowAnalog = showAnalog;
+        invalidate();
+    }
+
+    public boolean isShowAnalog() {
+        return mShowAnalog;
+    }
+
+}
